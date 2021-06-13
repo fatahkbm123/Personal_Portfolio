6d24e799468a3d34918c84dbd3a955e6003558c1
Fatah Noer Arbain <muhammadfatah398@gmail.com>
2021-06-13 20:49:29 +0700

    menambah beberapa bagian

---
+++ script.js
@@ -0,0 +1,29 @@
+const navbar = document.querySelector('#navbar')
+const navbarA = document.querySelectorAll('#navbar a')
+const section = document.querySelectorAll('section')
+
+window.addEventListener('scroll', function () {
+  let currentSection = ''
+
+  section.forEach((s) => {
+    const sectionTop = s.offsetTop
+
+    if (pageYOffset >= sectionTop) {
+      currentSection = s.getAttribute('id')
+    }
+  })
+
+  navbarA.forEach((a) => {
+    a.classList.remove('active')
+    if (a.classList.contains(currentSection)) {
+      a.classList.add('active')
+    }
+  })
+
+  // Navbar background animation
+  if (pageYOffset > 0) {
+    navbar.classList.add('active')
+  } else {
+    navbar.classList.remove('active')
+  }
+})
